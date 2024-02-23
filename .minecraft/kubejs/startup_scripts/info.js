30865e0429507e9660d97da7671ce107f476ba32
youyihj <youyi580@qq.com>
2024-02-24 00:26:57 +0800

    basic ingameinfo

---
+++ .minecraft/kubejs/startup_scripts/info.js
@@ -0,0 +1,23 @@
+if (Platform.isClientEnvironment()) {
+    console.log('register client event handler')
+
+    onForgeEvent('net.minecraftforge.client.event.RenderGameOverlayEvent$Text', event => {
+        const mc = java('net.minecraft.client.Minecraft').getInstance()
+        const fpsPattern = /(\d+) fps/
+        if (mc.options.renderDebug) return
+        var time = mc.level.dayTime()
+        var day = Math.floor(time / 24000)
+        var timeOfDay = time % 24000
+        var hour = Math.floor(timeOfDay / 1000) + 6;
+        if (hour >= 24) {
+            hour -= 24
+        }
+        var minute = Math.round((timeOfDay % 1000) * 0.06)
+        event.left.add(`Day ${day} ${hour < 10 ? '0' + hour : hour}:${minute < 10 ? '0' + minute : minute}`)
+        var match = mc.fpsString.match(fpsPattern)
+        if (match && match.length > 1) {
+            event.left.add(`FPS: ${match[1]}`)
+        }
+    })
+
+}
